4df69af4510b5b381b3c2350f607a460f7addc5a
Dave O'Connor <docgithub@dead-pixels.org>
2025-02-26 15:06:10 -0800

    Added override option for Commit Author names to use User Usernames (display_names) (#1631)

--- mailing_list/models.py
+++ mailing_list/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from mailing_list.managers import EmailDataManager
@@ -7,6 +8,15 @@ class EmailData(models.Model):
     author = models.ForeignKey("libraries.CommitAuthor", on_delete=models.CASCADE)
     version = models.ForeignKey("versions.Version", on_delete=models.CASCADE)
     count = models.IntegerField()
+
+    @property
+    def display_name(self):
+        return getattr(self.user, "display_name", None)
+
+    @property
+    def user(self):
+        User = get_user_model()
+        return User.get_user_by_github_url(self.author.github_profile_url)
 
     objects = EmailDataManager()
 
